dbf91a9b1a2b910c264ac2b6c4f7fe1df1d22c3e
AristideI <a.isingizwe@alustudent.com>
2023-10-09 18:56:59 +0200

    updates

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -5,12 +5,37 @@
  * @param {string} password
  * @returns {number}
  */
+
 export default function penaltyPoints(password = "") {
-  // The following line ensures, that password is always a string, like the number 128 -> string "128"
+  // Ensure that password is always a string
+  if (!password) return 0;
   if (typeof password !== "string") password = String(password);
 
-  // * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-  // * * * INSERT YOUR CODE HERE * * * * * * * * * * * * * *
-  // * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-  //
+  let points = 0;
+  let prevChar = null;
+  let seqLength = 0;
+
+  for (let i = 0; i < password.length; i++) {
+    const currChar = password[i];
+    if (currChar === prevChar) {
+      seqLength++;
+    } else {
+      if (seqLength === 2) {
+        points++;
+      } else if (seqLength >= 3) {
+        points += 2;
+      }
+      seqLength = 1; // reset the sequence length
+    }
+    prevChar = currChar;
+  }
+
+  // Handle the end of the string
+  if (seqLength === 2) {
+    points++;
+  } else if (seqLength >= 3) {
+    points += 2;
+  }
+
+  return points;
 }
